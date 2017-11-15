c11bc2c738cd49c5374b9295854d95ed0c16dcc0
Dayu Bian <dayub@chinasystems.com>
2017-11-15 13:51:41 +0800

    construct get invoices API

--- src/main/webapp/js/invoiceColumnApp.js
+++ src/main/webapp/js/invoiceColumnApp.js
@@ -1,11 +1,6 @@
 const app = angular.module('invoiceAppModule', ['ui.bootstrap']);
 
-// Fix for unhandled rejections bug.
-app.config(['$qProvider', function ($qProvider) {
-    $qProvider.errorOnUnhandledRejections(false);
-}]);
-
-app.controller('InvoiceAppController', function ($http, $location, $uibModal, $scope) {
+app.controller('InvoiceAppController', function ($http, $scope) {
     const invApp = this;
 
     // We identify the node.
@@ -18,90 +13,56 @@ app.controller('InvoiceAppController', function ($http, $location, $uibModal, $s
     };
 
     invApp.invoices = [
-        {x: new Date(2017, 01, 1), y: 1100, label: "INV17000001"},
-        {x: new Date(2017, 01, 2), y: 1200, label: "INV17000002"},
-        {x: new Date(2017, 01, 2), y: 1250, label: "INV17000003"},
-        {x: new Date(2017, 01, 3), y: 1280, label: "INV17000004"},
-        {x: new Date(2017, 01, 4), y: 1600, label: "INV17000005"},
+        {x: new Date(2017, 01, 1), y: 1100, indexLabel: "INV17000001"},
+        {x: new Date(2017, 01, 2), y: 1200, indexLabel: "INV17000002"},
+        {x: new Date(2017, 01, 2), y: 1250, indexLabel: "INV17000003"},
+        {x: new Date(2017, 01, 3), y: 1280, indexLabel: "INV17000004"},
+        {x: new Date(2017, 01, 4), y: 1600, indexLabel: "INV17000005"},
 
-        {x: new Date(2017, 01, 5), y: 2200, label: "INV17000006"},
-        {x: new Date(2017, 01, 6), y: 2200, label: "INV17000007"},
-        {x: new Date(2017, 01, 7), y: 2200, label: "INV17000008"},
-        {x: new Date(2017, 01, 8), y: 2200, label: "INV17000009"},
-        {x: new Date(2017, 01, 9), y: 2530, label: "INV17000010"},
-        {x: new Date(2017, 01, 11), y: 3040, label: "INV17000011"},
+        {x: new Date(2017, 01, 5), y: 2200, indexLabel: "INV17000006"},
+        {x: new Date(2017, 01, 6), y: 2200, indexLabel: "INV17000007"},
+        {x: new Date(2017, 01, 7), y: 2200, indexLabel: "INV17000008"},
+        {x: new Date(2017, 01, 8), y: 2200, indexLabel: "INV17000009"},
+        {x: new Date(2017, 01, 9), y: 2530, indexLabel: "INV17000010"},
+        {x: new Date(2017, 01, 11), y: 3040, indexLabel: "INV17000011"},
 
-        {x: new Date(2017, 01, 13), y: 4030, label: "INV17000012"},
-        {x: new Date(2017, 01, 14), y: 3040, label: "INV17000013"},
-        {x: new Date(2017, 01, 18), y: 4060, label: "INV17000014"},
-        {x: new Date(2017, 01, 20), y: 4040, label: "INV17000015"},
-        {x: new Date(2017, 01, 22), y: 5100, label: "INV17000016"},
-        {x: new Date(2017, 01, 23), y: 4200, label: "INV17000017"},
-        {x: new Date(2017, 01, 23), y: 3030, label: "INV17000018"},
-        {x: new Date(2017, 01, 23), y: 3020, label: "INV17000019"},
+        {x: new Date(2017, 01, 13), y: 4030, indexLabel: "INV17000012"},
+        {x: new Date(2017, 01, 14), y: 3040, indexLabel: "INV17000013"},
+        {x: new Date(2017, 01, 18), y: 4060, indexLabel: "INV17000014"},
+        {x: new Date(2017, 01, 20), y: 4040, indexLabel: "INV17000015"},
+        {x: new Date(2017, 01, 22), y: 5100, indexLabel: "INV17000016"},
+        {x: new Date(2017, 01, 23), y: 4200, indexLabel: "INV17000017"},
+        {x: new Date(2017, 01, 23), y: 3030, indexLabel: "INV17000018"},
+        {x: new Date(2017, 01, 23), y: 3020, indexLabel: "INV17000019"},
 
-        {x: new Date(2017, 01, 28), y: 8210, label: "INV17000020"},
-        {x: new Date(2017, 01, 31), y: 8040, label: "INV17000021"},
-        {x: new Date(2017, 02, 1), y: 9060, label: "INV17000022"},
-        {x: new Date(2017, 02, 4), y: 8300, label: "INV17000023"},
-        {x: new Date(2017, 02, 5), y: 9300, label: "INV17000024"},
-        {x: new Date(2017, 02, 7), y: 6300, label: "INV17000025"},
-        {x: new Date(2017, 02, 11), y: 9900, label: "INV17000026"},
-        {x: new Date(2017, 02, 20), y: 4200, label: "INV17000027"},
-        {x: new Date(2017, 02, 28), y: 8200, label: "INV17000028"},
-
-        {x: new Date(2017, 03, 5), y: 9040, label: "INV17000029"},
-        {x: new Date(2017, 03, 8), y: 9200, label: "INV17000030"},
-        {x: new Date(2017, 03, 11), y: 7020, label: "INV17000031"},
-        {x: new Date(2017, 03, 19), y: 9040, label: "INV17000032"},
-        {x: new Date(2017, 03, 20), y: 9030, label: "INV17000033"},
-        {x: new Date(2017, 03, 25), y: 10120, label: "INV17000034"},
-        {x: new Date(2017, 03, 30), y: 4020, label: "INV17000035"},
-
-        {x: new Date(2017, 04, 5), y: 13400, label: "INV17000036"}
+        {x: new Date(2017, 01, 28), y: 8210, indexLabel: "INV17000020"},
+        {x: new Date(2017, 01, 31), y: 8040, indexLabel: "INV17000021"}
     ];
 
     // invApp.dataPoints should be derived from invApp.invoices
     invApp.dataPoints = [
-        {x: new Date(2017, 01, 1), y: 1100, label: 1},
-        {x: new Date(2017, 01, 2), y: 2250, label: 2},
+        {x: new Date(2017, 01, 1), y: 1100, indexLabel: "1"},
+        {x: new Date(2017, 01, 2), y: 2250, indexLabel: "2"},
 
-        {x: new Date(2017, 01, 3), y: 1280, label: 1},
-        {x: new Date(2017, 01, 4), y: 1600, label: 1},
+        {x: new Date(2017, 01, 3), y: 1280, indexLabel: "1"},
+        {x: new Date(2017, 01, 4), y: 1600, indexLabel: "1"},
 
-        {x: new Date(2017, 01, 5), y: 2200, label: 1},
-        {x: new Date(2017, 01, 6), y: 2200, label: 1},
-        {x: new Date(2017, 01, 7), y: 2200, label: 1},
-        {x: new Date(2017, 01, 8), y: 2200, label: 1},
-        {x: new Date(2017, 01, 9), y: 2530, label: 1},
-        {x: new Date(2017, 01, 11), y: 3040, label: 1},
+        {x: new Date(2017, 01, 5), y: 2200, indexLabel: "1"},
+        {x: new Date(2017, 01, 6), y: 2200, indexLabel: "1"},
+        {x: new Date(2017, 01, 7), y: 2200, indexLabel: "1"},
+        {x: new Date(2017, 01, 8), y: 2200, indexLabel: "1"},
+        {x: new Date(2017, 01, 9), y: 2530, indexLabel: "1"},
+        {x: new Date(2017, 01, 11), y: 3040, indexLabel: "1"},
 
-        {x: new Date(2017, 01, 13), y: 4030, label: 1},
-        {x: new Date(2017, 01, 14), y: 3040, label: 1},
-        {x: new Date(2017, 01, 18), y: 4060, label: 1},
-        {x: new Date(2017, 01, 20), y: 4040, label: 1},
-        {x: new Date(2017, 01, 22), y: 5100, label: 1},
-        {x: new Date(2017, 01, 23), y: 10250, label: 3},
+        {x: new Date(2017, 01, 13), y: 4030, indexLabel: "1"},
+        {x: new Date(2017, 01, 14), y: 3040, indexLabel: "1"},
+        {x: new Date(2017, 01, 18), y: 4060, indexLabel: "1"},
+        {x: new Date(2017, 01, 20), y: 4040, indexLabel: "1"},
+        {x: new Date(2017, 01, 22), y: 5100, indexLabel: "1"},
+        {x: new Date(2017, 01, 23), y: 10250, indexLabel: "3"},
 
-        {x: new Date(2017, 01, 28), y: 8210, label: 1},
-        {x: new Date(2017, 01, 31), y: 8040, label: 1},
-        {x: new Date(2017, 02, 1), y: 9060, label: 1},
-        {x: new Date(2017, 02, 4), y: 8300, label: 1},
-        {x: new Date(2017, 02, 5), y: 9300, label: 1},
-        {x: new Date(2017, 02, 7), y: 6300, label: 1},
-        {x: new Date(2017, 02, 11), y: 9900, label: 1},
-        {x: new Date(2017, 02, 20), y: 4200, label: 1},
-        {x: new Date(2017, 02, 28), y: 8200, label: 1},
-
-        {x: new Date(2017, 03, 5), y: 9040, label: 1},
-        {x: new Date(2017, 03, 8), y: 9200, label: 1},
-        {x: new Date(2017, 03, 11), y: 7020, label: 1},
-        {x: new Date(2017, 03, 19), y: 9040, label: 1},
-        {x: new Date(2017, 03, 20), y: 9030, label: 1},
-        {x: new Date(2017, 03, 25), y: 10120, label: 1},
-        {x: new Date(2017, 03, 30), y: 4020, label: 1},
-
-        {x: new Date(2017, 04, 5), y: 13400, label: 1}
+        {x: new Date(2017, 01, 28), y: 8210, indexLabel: "1"},
+        {x: new Date(2017, 01, 31), y: 8040, indexLabel: "1"}
     ];
 
     invApp.selectedInvoices = [];
@@ -113,7 +74,9 @@ app.controller('InvoiceAppController', function ($http, $location, $uibModal, $s
             },
             axisX: {
                 title: "timeline",
-                gridThickness: 2
+                gridThickness: 2,
+                interval: 1,
+                intervalType: "day"
             },
             axisY: {
                 title: "face value (USD)"
@@ -126,7 +89,7 @@ app.controller('InvoiceAppController', function ($http, $location, $uibModal, $s
                         $scope.$apply();
                     },
                     type: "column",
-                    toolTipContent: "date: {x} <hr/> No. of Invoices: {label}, total: {y}",
+                    toolTipContent: "date: {x} <br/> No. of Invoices: {indexLabel}, total: {y}",
                 }
             ]
         });
--- src/main/webapp/js/invoiceScatterApp.js
+++ src/main/webapp/js/invoiceScatterApp.js
@@ -1,19 +1,16 @@
 const app = angular.module('invoiceAppModule', ['ui.bootstrap']);
 
-// Fix for unhandled rejections bug.
-app.config(['$qProvider', function ($qProvider) {
-    $qProvider.errorOnUnhandledRejections(false);
-}]);
-
-app.controller('InvoiceAppController', function ($http, $location, $uibModal, $scope) {
+app.controller('InvoiceAppController', function ($http, $scope) {
     const invApp = this;
 
-    // We identify the node.
-    const apiBaseURL = "/api/example/";
+    // Identify the rest api node.
+    const apiBaseURL = "http://localhost:9080/rest/invoice";
+
+    invApp.invoices = [];
 
     invApp.selectedInv = {
         referenceNumber: "",
-        faceValue: 0,
+        faceValueInUSD: 0,
         maturityDate: Date(2017, 01, 1)
     };
 
@@ -24,7 +21,8 @@ app.controller('InvoiceAppController', function ($http, $location, $uibModal, $s
             },
             axisX: {
                 title: "timeline",
-                gridThickness: 2
+                interval: 1,
+                intervalType: "day"
             },
             axisY: {
                 title: "face value (USD)"
@@ -33,63 +31,36 @@ app.controller('InvoiceAppController', function ($http, $location, $uibModal, $s
                 {
                     click: function (e) {
 
-                        invApp.selectedInv.referenceNumber = e.dataPoint.label;
-                        invApp.selectedInv.faceValue = e.dataPoint.y;
+                        invApp.selectedInv.referenceNumber = e.dataPoint.name;
+                        invApp.selectedInv.faceValueInUSD = e.dataPoint.y;
                         invApp.selectedInv.maturityDate = e.dataPoint.x;
 
                         //alert("dataSeries Event => Type: " + e.dataSeries.type + ", dataPoint { x:" + invApp.selectedInv.maturityDate + ", y: " + invApp.selectedInv.faceValue + ", label: " + invApp.selectedInv.referenceNumber + " }");
                         $scope.$apply();
                     },
                     type: "scatter",
-                    toolTipContent: "{label} <hr/> date: {x}, value: {y}",
-                    dataPoints: [
-
-                        {x: new Date(2017, 01, 1), y: 1100, label: "INV17000001"},
-                        {x: new Date(2017, 01, 2), y: 1200, label: "INV17000002"},
-                        {x: new Date(2017, 01, 2), y: 1250, label: "INV17000003"},
-                        {x: new Date(2017, 01, 3), y: 1280, label: "INV17000004"},
-                        {x: new Date(2017, 01, 4), y: 1600, label: "INV17000005"},
-
-                        {x: new Date(2017, 01, 5), y: 2200, label: "INV17000006"},
-                        {x: new Date(2017, 01, 6), y: 2200, label: "INV17000007"},
-                        {x: new Date(2017, 01, 7), y: 2200, label: "INV17000008"},
-                        {x: new Date(2017, 01, 8), y: 2200, label: "INV17000009"},
-                        {x: new Date(2017, 01, 9), y: 2530, label: "INV17000010"},
-                        {x: new Date(2017, 01, 11), y: 3040, label: "INV17000011"},
-
-                        {x: new Date(2017, 01, 13), y: 4030, label: "INV17000012"},
-                        {x: new Date(2017, 01, 14), y: 3040, label: "INV17000013"},
-                        {x: new Date(2017, 01, 18), y: 4060, label: "INV17000014"},
-                        {x: new Date(2017, 01, 20), y: 4040, label: "INV17000015"},
-                        {x: new Date(2017, 01, 22), y: 5100, label: "INV17000016"},
-                        {x: new Date(2017, 01, 23), y: 4200, label: "INV17000017"},
-                        {x: new Date(2017, 01, 23), y: 3030, label: "INV17000018"},
-                        {x: new Date(2017, 01, 23), y: 3020, label: "INV17000019"},
-
-                        {x: new Date(2017, 01, 28), y: 8210, label: "INV17000020"},
-                        {x: new Date(2017, 01, 31), y: 8040, label: "INV17000021"},
-                        {x: new Date(2017, 02, 1), y: 9060, label: "INV17000022"},
-                        {x: new Date(2017, 02, 4), y: 8300, label: "INV17000023"},
-                        {x: new Date(2017, 02, 5), y: 9300, label: "INV17000024"},
-                        {x: new Date(2017, 02, 7), y: 6300, label: "INV17000025"},
-                        {x: new Date(2017, 02, 11), y: 9900, label: "INV17000026"},
-                        {x: new Date(2017, 02, 20), y: 4200, label: "INV17000027"},
-                        {x: new Date(2017, 02, 28), y: 8200, label: "INV17000028"},
-
-                        {x: new Date(2017, 03, 5), y: 9040, label: "INV17000029"},
-                        {x: new Date(2017, 03, 8), y: 9200, label: "INV17000030"},
-                        {x: new Date(2017, 03, 11), y: 7020, label: "INV17000031"},
-                        {x: new Date(2017, 03, 19), y: 9040, label: "INV17000032"},
-                        {x: new Date(2017, 03, 20), y: 9030, label: "INV17000033"},
-                        {x: new Date(2017, 03, 25), y: 10120, label: "INV17000034"},
-                        {x: new Date(2017, 03, 30), y: 4020, label: "INV17000035"},
-
-                        {x: new Date(2017, 04, 5), y: 13400, label: "INV17000036"}
-                    ]
+                    toolTipContent: "Ref No.: {name} <br/> date: {x}, value: {y}"
                 }
             ]
         });
 
-    invApp.chart.render(); //render the chart for the first time
+    invApp.chart.options.data[0].dataPoints = [];
+
+    // Get invoices
+    $http.get(apiBaseURL)
+        .then(function success(response) {
+            invApp.invoices = response.data;
+            invApp.invoices.forEach(function(item){
+                invApp.chart.options.data[0].dataPoints.push({x: new Date(item.maturityDate), y: item.faceValueInUSD, name: item.referenceNumber});
+            });
+            //invApp.chart.options.data[0].dataPoints = invApp.invoices;
+            //alert("invoice number: " + invApp.chart.options.data[0].dataPoints.length);
+            //alert("invoice[0]" + invApp.chart.options.data[0].dataPoints[0].x
+            //    + " - " + invApp.chart.options.data[0].dataPoints[0].y
+            //    + " - " + invApp.chart.options.data[0].dataPoints[0].label);
+            invApp.chart.render();
+        },function error(response) {
+            alert("Cannot get data from server!" + response);
+        });
 
 });
